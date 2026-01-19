4c49450f89970af3252fa6cc065d8a2ed67162b4
appmonster007 <13420432+appmonster007@users.noreply.github.com>
2026-01-19 21:52:12 +0530

    feat: implement CreateKeyPairOpRequestPayload structure
    Add CreateKeyPairOpRequestPayload model, codecs, tests, and benchmark subject.
    Register new types and codecs in service loader files.

---
+++ src/test/java/org/purpleBean/kmip/codec/xml/model/v1_2/structure/request/CreateKeyPairOpRequestPayloadXmlTest.java
@@ -0,0 +1,40 @@
+package org.purpleBean.kmip.codec.xml.model.v1_2.structure.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.purpleBean.kmip.api.KmipSpec;
+import org.purpleBean.kmip.model.core.structure.CommonTemplateAttribute;
+import org.purpleBean.kmip.model.core.structure.PrivateKeyTemplateAttribute;
+import org.purpleBean.kmip.model.core.structure.PublicKeyTemplateAttribute;
+import org.purpleBean.kmip.model.v1_2.structure.request.CreateKeyPairOpRequestPayload;
+import org.purpleBean.kmip.test.suite.AbstractXmlSerializationTestSuite;
+
+@DisplayName("CreateKeyPairOpRequestPayload Xml Serialization Tests")
+class CreateKeyPairOpRequestPayloadXmlTest extends AbstractXmlSerializationTestSuite<CreateKeyPairOpRequestPayload> {
+
+    @Override
+    protected void setupDefaultSpec() {
+        defaultSpec = KmipSpec.UnknownVersion;
+    }
+
+    @Override
+    protected Class<CreateKeyPairOpRequestPayload> type() {
+        return CreateKeyPairOpRequestPayload.class;
+    }
+
+    @Override
+    protected CreateKeyPairOpRequestPayload createDefault() {
+        return CreateKeyPairOpRequestPayload.builder()
+                .commonTemplateAttribute(CommonTemplateAttribute.builder().build())
+                .privateKeyTemplateAttribute(PrivateKeyTemplateAttribute.builder().build())
+                .publicKeyTemplateAttribute(PublicKeyTemplateAttribute.builder().build())
+                .build();
+    }
+
+    @Override
+    protected CreateKeyPairOpRequestPayload createVariant() {
+        return CreateKeyPairOpRequestPayload.builder()
+                .commonTemplateAttribute(CommonTemplateAttribute.builder().build())
+                .privateKeyTemplateAttribute(PrivateKeyTemplateAttribute.builder().build())
+                .build();
+    }
+}
